eeb278fa2a23a24787817ae755c3ff449246fc66
Chandrek Bisht <chandrek07@gmail.com>
2024-06-04 05:14:39 +0530

    api commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,27 +1,27 @@
-import axios from 'axios';
-
-const API_URL = 'https://sonic-backend-mr12.onrender.com';
-const AUTH_TOKEN = process.env.REACT_APP_STRIPE_DEV_APP_KEY;
+import axios from "axios";
 
 const params = {
     headers: {
-        Authorization: `Bearer ${AUTH_TOKEN}`,
+        Authorization: "bearer " + process.env.REACT_APP_STRIPE_DEV_APP_KEY,
     },
 };
 
 export const fetchDataFromApi = async (url) => {
     try {
-        const {data} = await axios.get(`${API_URL}${url}`, params);
+        const {data} = await axios.get(
+            process.env.REACT_APP_STRIPE_APP_DEV_URL + url,
+            params
+        );
         return data;
     } catch(err) {
-        console.error(err);
+        console.log(err);
         return err;
     }
 };
 
 export const makePaymentRequest = axios.create({
-    baseURL: API_URL,
+    baseURL: process.env.REACT_APP_STRIPE_APP_DEV_URL,
     headers: {
-        Authorization: `Bearer ${AUTH_TOKEN}`,
+        Authorization: "bearer " + process.env.REACT_APP_STRIPE_DEV_APP_KEY,
     },
 });
